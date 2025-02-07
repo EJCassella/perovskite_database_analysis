71b1e3c7614cc7ac190f9b077d4b2b0a3bb32efe
Elena Josephine Cassella <elenacassella@sky.com>
2025-02-07 17:09:24 +0000

    Added more logging information.

--- pipeline.py
+++ pipeline.py
@@ -4,6 +4,7 @@ from utils import setup_logger
 if __name__ == '__main__':
   logger = setup_logger()
   df = extract('Perovskite_database_content_all_data accessed_2025-01-22.csv')
+  logger.info('Beginning data cleaning and transformation steps.')
   cleaned_df = transform(df)
   load(cleaned_df)
   logger.info('Finished!')
